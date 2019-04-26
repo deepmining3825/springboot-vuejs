48d772149f427c250e5bd0c9aa17157c907cfbe5
deepmining3825 <deepmining3825@gmail.com>
2019-04-26 10:19:37 +0900

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="callRestService">call</button>
+  </div>
+</template>
+
+<script>
+  // import axios from 'axios'
+  import { AXIOS } from './components/http-common.js'
+  export default {
+    name: 'service',
+    data () {
+      return {
+        msg: 'HowTo call REST-Services:',
+        response: [],
+        errors: []
+      }
+    },
+    methods: {
+      // Fetches posts when the component is created.
+      callRestService () {
+        AXIOS.get(`/hello`)
+                .then(response => {
+                  // JSON responses are automatically parsed.
+                  this.response = response.data;
+                  console.log(response.data)
+                })
+                .catch(e => {
+                  this.errors.push(e)
+                })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
